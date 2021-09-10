d29090034c0440d2c3d06a6a381f895d0819e437
Zakaria Abdelaziz <35163329+cryptic-dev@users.noreply.github.com>
2021-09-11 00:19:31 +0100

    fixed

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-
 const form = document.getElementById("form");
 const main = document.getElementById("main");
 const search = document.getElementById("search");
 
-
-
 const apiKey = "8ae8b005d87a451c517c7f6e5ed388f1";
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-async function getWeather(){
-    const resp = await fetch(url);
-    const temp = await resp.json();
-    console.log(temp.main.temp)
+async function getWeather(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  const resp = await fetch(url);
+  const data = await resp.json();
+
+  updateTemp(data);
 }
 
+function updateTemp(data) {
+  const temperature = document.getElementById("temp");
+  const icon = document.getElementById("icon");
 
-function addelement() {
-    const temperature = document.createElement("div");
-    temperature.classList.add("temperature");
+  if (data.main) {
+    //update text
+    temperature.innerHTML = `<p> 
+        Temp in <span style="color:green">
+        ${data.name},
+        ${data.sys.country}
+        </span> is <span style="color:green">
+        ${Math.round(data.main.temp, 10)}°</span>.`;
 
-    temperature.innerHTML = 
-    `
-    <p> the temp is ${temp.main.temp} for now    `;
-    main.appendChild(temperature);
+    //update icon
+    icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  } else {
+    temperature.innerHTML = `<p style="color:red;">${data.message}</p>`;
+
+    icon.src =
+      "https://cdn.iconscout.com/icon/premium/png-256-thumb/error-2547221-2136999.png";
+  }
 }
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const city = search.value;
-    if (city) {
-        getWeather(city)
-    }
+  e.preventDefault();
+  const city = search.value;
 
-})
+  if (city) {
+    getWeather(city);
+  }
+});
